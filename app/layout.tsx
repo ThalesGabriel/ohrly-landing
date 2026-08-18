15802b5c34a08e98dd871bbf7977623dbce5a407
taraujo1@costar.com <taraujo1@costar.com>
2026-08-18 04:48:33 -0300

    novo regime

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body>
         {children}
         <Analytics />
-        <Script
-          src="https://www.app.ohrly.com.br/ohrly.js"
-          strategy="afterInteractive"
+        <script
+          async
+          src="https://app.ohrly.com.br/ohrly.js"
           data-project-key="pk_af23ee02a84cbb59a2e92d2a987358f88ddb"
           data-storage="session"
-        />
+          data-collection-mode="anonymous_aggregate"
+          data-observe-consent-events="true"
+          data-activation-requires-marketing-consent="true"
+        ></script>
       </body>
     </html>
   );
